6db1b91bde24386e8350ee56d3cc5b3231cb6f17
WillRMorris <153463826+WillRMorris@users.noreply.github.com>
2024-06-03 16:34:51 -0600

    replacing with a new base to include apollo

--- models/index.js
+++
@@ -1,4 +0,0 @@
-const Thought = require('./thought');
-const User = require('./user');
-
-module.exports = {Thought, User}
--- routes/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const thoughtRoutes = require('./thoughtRoutes');
-const UserRoutes = require('./userRoutes');
-
-router.use('/users', UserRoutes);
-router.use('/thoughts', thoughtRoutes)
-
-
-module.exports = router;
